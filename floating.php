033bd962f755af228089c1b6a0d83fe86a820c52
ramaprixim <rama.prixim@gmail.com>
2024-03-11 18:15:48 +0530

    first commit

--- floating.php
+++ floating.php
@@ -1,77 +1,47 @@
 <?php
-header('Access-Control-Allow-Origin: https://greylabel-fx.com/');
 if(isset($_POST['submit'])) {
- 
     $email_to = "sales@greylabel-fx.com";
+    $email_subject = "Contact Us Form";
     
-    $email_subject = "Contact Us Form ";
- 
-    function died($error) {
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
+    // Check if required fields are set and not empty
+    if(empty($_POST['float-name']) ||
+       empty($_POST['float-email']) ||
+       empty($_POST['msg'])) {
+        die('We are sorry, but there appears to be a problem with the form you submitted.');
     }
- 
- 
-    if(!isset($_POST['float-name']) ||
-        !isset($_POST['float-email']) ||
-        
-       
-        !isset($_POST['msg'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
- 
-
-     
+    
     $name = $_POST['float-name']; 
-    
     $email_from = $_POST['float-email']; 
-    
-    
     $comments = $_POST['msg']; 
     $ip = $_SERVER['REMOTE_ADDR']; 
- 
-
- 
+    
+    // Construct email message
     $email_message = "Form details below.\n\n";
- 
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-    $email_message .= "Senders IP: ".clean_string($ip)."\n\n"; 
-    $email_message .= "Name: ".clean_string($name)."\n";
-   
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-   
-   
-    $email_message .= "Requirements: ".clean_string($comments)."\n";
+    $email_message .= "Senders IP: ".$ip."\n\n"; 
+    $email_message .= "Name: ".$name."\n";
+    $email_message .= "Email: ".$email_from."\n";
+    $email_message .= "Requirements: ".$comments."\n";
     
-      
+    // Set headers
+    $headers = 'From: noreply@greylabel-fx.com'."\r\n";
+    $headers .= 'Reply-To: '.$email_from."\r\n" .
+                'X-Mailer: PHP/' . phpversion();
     
-$headers = 'From: noreply@greylabel-fx.com'."\r\n";
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-
-if (@mail($email_to, $email_subject, $email_message, $headers))
-	  {
-         ?>
-    	<script language="javascript" type="text/javascript">
-    		alert('Thank you for the message. We will contact you shortly.');
-    		window.location = 'index.html';
-    	</script>
-    <?php
+    // Send email
+    if (@mail($email_to, $email_subject, $email_message, $headers)) {
+        ?>
+        <script language="javascript" type="text/javascript">
+            alert('Thank you for the message. We will contact you shortly.');
+            window.location = 'index.html';
+        </script>
+        <?php
+    } else {
+        ?>
+        <script language="javascript" type="text/javascript">
+            alert('Message failed. Please, send an email to customercare@prixim.co.uk');
+            window.location = 'index.html';
+        </script>
+        <?php
     }
-    else { ?>
-    	<script language="javascript" type="text/javascript">
-    		alert('Message failed. Please, send an email to customercare@prixim.co.uk');
-    		window.location = 'index.html';
-    	</script>
-    <?php
-    }
-   }
+}
 ?>
